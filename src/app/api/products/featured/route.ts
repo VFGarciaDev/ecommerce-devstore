16e70be9adcbe2dd96f7d1802a54c0d6f89c95bd
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-22 14:50:57 -0300

    feat: api products

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,6 @@
+import data from '../data.json'
+
+export function GET() {
+    const featuredProducts = data.products.filter(product => product.featured)
+    return Response.json(featuredProducts)
+}
