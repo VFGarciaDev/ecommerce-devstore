0efd1894b208f557b9f5aa7969442b123480c79a
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-25 10:45:01 -0300

    feat: skeleton component

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function HomeLoading() {
-    return <p>Carregando...</p>
+    return (
+        <div className="h-full grid grid-cols-[2fr_1fr] gap-6">
+            <Skeleton className="row-span-2 h-[800px]" />
+            <Skeleton />
+            <Skeleton />
+        </div>
+    )
 }
