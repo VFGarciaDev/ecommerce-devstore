4a2877ed9682409cf4060616ed456a805e586b0c
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-25 16:07:27 -0300

    fetch de dados

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -4,33 +4,52 @@ import Image from "next/image";
 
 const sizes = ["P", "M", "G", "GG"]
 
-async function getProductDetails(): Promise<Product> {
-    const response = await api('/products/[slug]', {
+type ProductProps = {
+    params: {
+        slug: string
+    }
+}
+
+async function getProductDetails(slug: string): Promise<Product> {
+    const response = await api(`/products/${slug}`, {
         next: {
-            revalidate: 10
+            revalidate: 60 * 60
         }
     })
     const product = await response.json()
     return product
 }
 
-export default async function ProductPage(props: any) {
-    const p = await getProductDetails()
-    console.log(p)
+export default async function ProductPage({ params }: ProductProps) {
+    const product = await getProductDetails(params.slug)
+    console.log(product)
+
+    const price = (split: number = 1) => {
+        return (product.price / split)
+            .toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                minimumFractionDigits: split > 1 ? 2 : 0,
+                maximumFractionDigits: split > 1 ? 2 : 0,
+            })
+    }
 
     return (
         <main className="grid grid-cols-[3fr_2fr]">
-            <Image src="/" alt=""
+            <Image src={product.image} alt={product.title}
                 width={1024} height={1024} quality={100}
                 className="overflow-hidden" />
             <aside className="flex flex-col justify-center gap-8 px-12">
                 <section>
-                    <h1 className="text-3xl font-bold leading-tight">{JSON.stringify(props)}</h1>
-                    <p className="mt-2 leading-relaxed text-zinc-400"></p>
+                    <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
+                    <p className="mt-2 leading-relaxed text-zinc-400">{product.description}</p>
                 </section>
                 <div className="flex items-center gap-3">
-                    <span className="inline-block text-nowrap py-3 px-5 bg-violet-500 rounded-full font-semibold">124</span>
-                    <span>Em 12x s/ juros de R$</span>
+                    <span
+                        className="inline-block text-nowrap py-3 px-5 bg-violet-500 rounded-full font-semibold">
+                        {price()}
+                    </span>
+                    <span>Em até 12x s/ juros de {price(12)}</span>
                 </div>
                 <section className="font-semibold">
                     <h2 className="text-lg">Tamanhos</h2>
--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,15 +1,15 @@
 import data from '../data.json'
 import { z } from 'zod'
 
-export async function GET(_: Request, { params }: {params: {slug: string}}) {
+export async function GET(_: Request, { params }: { params: { slug: string } }) {
     // ohter: { params: { slug: string}}
     await new Promise(resolve => setTimeout(resolve, 1000))
 
-    const slug = z.string().parse(params.slug)
+    const slug = z.string().parse(params.slug) // pase != safeParse -> safeParse não envia msg Erro se estiver esrrado
     const product = data.products.find(product => product.slug === slug)
 
-    if(!product) {
-        return Response.json({message: 'Product not found', status: 400})
+    if (!product) {
+        return Response.json({ message: 'Product not found' }, { status: 400 })
     }
     return Response.json(product)
 }
