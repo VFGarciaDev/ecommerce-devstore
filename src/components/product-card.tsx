8da82872b44a7fbe2a7d5e8b6f427b84f66b27cb
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-07-25 10:45:45 -0300

    feat: product card component

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -3,16 +3,17 @@ import Image from "next/image"
 import Link from "next/link"
 
 type ProductCardProps = {
+    slug: string,
     image: string,
     name: string,
     value: string,
     mainCard?: boolean,
 }
 
-export const ProductCard = ({image, name, value, mainCard}:ProductCardProps) => {
+export const ProductCard = ({slug, image, name, value, mainCard}:ProductCardProps) => {
 
     return (
-        <Link href="/" className={cn("group relative bg-zinc-900 overflow-hidden", mainCard && "row-span-2")}>
+        <Link href={`/product/${slug}`} className={cn("group relative bg-zinc-900 overflow-hidden", mainCard && "row-span-2")}>
             <Image src={image} width={920} height={920} quality={100} alt="" className="size-full group-hover:scale-105 transition-transform duration-500" />
             <div className={
                 cn("absolute flex items-center gap-2 h-12 max-w-[280px] bg-black/60 border-2 border-zinc-500 rounded-full p-1 pl-4", 
