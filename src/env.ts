22c4734fa62212248fb1bc7dfb29bd33ac814a36
Victor Garcia <vfeitosa.garcia@gmail.com>
2024-08-07 11:03:35 -0300

    feat: add client/server env variables boundaries

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,25 @@
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
-const envSchema = z.object({
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+export const env = createEnv({
+  /* Serverside Environment variables, not available on the client.
+  Will throw if you access these variables on the client. */
+  server: {
     APP_URL: z.string().url()
-})
+  },
 
-const parsedEnv = envSchema.safeParse(process.env)
+  /* Environment variables available on the client (and server).
+  You'll get type errors if these are not prefixed with NEXT_PUBLIC_. */
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url()
+  },
 
-if(!parsedEnv.success) {
-    console.error(
-        'Invalid environment variables',
-        parsedEnv.error.flatten().fieldErrors
-    )
-
-    throw new Error('Invalid environment variables.')
-}
-
-export const env = parsedEnv.data
+  /* Due to how Next.js bundles environment variables on Edge and Client,
+  * we need to manually destructure them to make sure all are included in bundle.
+  You'll get type errors if not all variables from `server` & `client` are included here. */
+   
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+});
